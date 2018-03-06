f846689846f37ace9caebb9c24dc1315818e2a91
RajuJanne <moby@insomnia.fi>
2018-03-06 20:17:33 +0200

    APPI

--- js/APPI.js
+++ js/APPI.js
@@ -1,84 +1,57 @@
-$(document).ready(function(){
-  $('.APPI-nappi').click(function (){
-    APPI.poistaPopup();
-    /* Tästä eteenpäin kannattaa pitää silmät kiinni jos on alaikäinen.
-       Keksein tämän ratkaisun itse ilman googlettelua. */
-    let likainenTemppu = APPI.komennot.indexOf($('.APPI-syote').val());
-    $('.APPI-syote').val("");
-    if (likainenTemppu > -1) { /* indexOf() palauttaa arvon -1 jos ei löydy */
-      eval(`APPI.f${likainenTemppu}()`);
-    }
-  });
-  $(document).keypress(function (e){
-    if (e.which === 13) {
-      APPI.poistaPopup();
-      let likainenTemppu = APPI.komennot.indexOf($('.APPI-syote').val());
-      $('.APPI-syote').val("");
-      if (likainenTemppu > -1) {
-        eval(`APPI.f${likainenTemppu}()`);
-      }
-    }
-  });
-});
-
-// kaikki f + numero muotoa olevat funktiot ovat mahdollisia komentoja.
-var APPI = {
-  f0: function() {
-    // listataa kaikki
+let funktiot = [
+  function() { // listataa kaikki
     APPI.luoPopup("list");
     for (var i = 0; i < APPI.komennot.length; i++) {
       // $('.popup').append(APPI.komennot[i] + "<br/>");
       APPI.taytaPopup(APPI.komennot[i] + "<br />");
     }
     APPI.luoNappi("oikAla", "palaa", "APPI.poistaPopup()");
-  },
-  f1: function() {
-    // google
+  },function() { // google
     APPI.luoPopup("google");
     APPI.taytaPopup("<p>Siirrytäänkö Googleen?</p>");
     APPI.luoNappi("gKyllä", "Kyllä", "window.location = 'https://google.com'");
-    APPI.luoNappi("gEi", "Ei","APPI.poistaPopup()")
-  },
-  f2: function() {
-    // janne
+    APPI.luoNappi("gEi", "Palaa", "APPI.poistaPopup()")
+  },function() { // janne
     APPI.luoPopup("janne");
     APPI.luoNappi("oikAla", "palaa", "APPI.poistaPopup()")
-  },
-  f3: function() {
-    // responsiivinen
+  },function() { // responsiivinen
     APPI.luoPopup("respo");
     APPI.taytaPopup("<p>Tää oli oikeesti ihan saatanan hauska kurssi, en ollu koskaan ennen koskenu mihinkään css:ää korkeempiin voimiin selaimissa. Kaikki tää kamaluus on siis tään kurssin innoittamana opittua >:3</p>");
-  },
-  f4: function() {
-    // mielenhallintakakkara
-  },
-  f5: function() {
-    // hans
-  },
-  f6: function() {
-    // akku
-  },
-  f7: function() {
-    // miinaharava
-  },
-  f8: function() {
-    // gentoo
-  },
-  f9: function() {
-    // kärpät
+    APPI.taytaPopup('<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FStaartOy%2Fvideos%2F10154505275659376%2F&show_text=0&width=476" class="nörttinurkka" width="476" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe><br />');
+    APPI.luoNappi("oikAla", "palaa", "APPI.poistaPopup()");
+  },function() { // mielenhallintakakkara
+  },function() { // hans
+  },function() { // akku
+  },function() { // miinaharava
+  },function() { // gentoo
+  },function() { // kärpät
     window.close();
-  },
-  f10: function () {
-    // peter
+  },function () { // peter
     APPI.luoPopup("ounou");
     APPI.taytaPopup("<p>EI OLLU HYVÄ IDEA</p><p>PS. Eevertti teki kuvam käsintelyt.</p>");
-  },
+    APPI.luoNappi("oikAla", "palaa", "APPI.poistaPopup()");
+  }
+]
+$(document).ready(function(){
+  $(document).keypress(function (e){
+    if (e.which === 13) {
+      APPI.poistaPopup();
+      let i = APPI.komennot.indexOf($('.APPI-syote').val());
+      if (i > -1) {
+        funktiot[i]();
+        $('.APPI-syote').val("");
+      }
+    }
+  });
+});
+var APPI = {
   luoPopup: function(v) {
     let popUp = `<div class="popup ${v}"></div>`;
     $('.APPI-inner').append(popUp);
   },
   poistaPopup: function() {
     $('.popup').remove();
+    $('.APPI-syote').focus(); /* Heitetään tähtäin takaisin hattuun */
   },
   taytaPopup: function(a) {
     $('.popup').append(a);
