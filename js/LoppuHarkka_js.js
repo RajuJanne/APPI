99b051a0be5046947ee7e56e7ef96c0e4f95669a
RajuJanne <moby@insomnia.fi>
2018-03-06 15:50:21 +0200

    likainen eval()

--- js/LoppuHarkka_js.js
+++ js/LoppuHarkka_js.js
@@ -1,23 +1,45 @@
 $(document).ready(function(){
   var APPI = {
     f0: function() {
-      // let googlepop = "<div id="googlePop">";
-      //     googlePop +=  "<img src='./images/kyngs.png' width=275px heigth=275px>";
+      // listataan kaikki kivasti johonki.
       console.log(komennot[0]);
     },
     f1: function() {
+      // google popuppi
 
     },
     f2: function() {
+      // oma nimesi
 
     },
     f3: function() {
+      // responsiivinen
 
     },
+    f4: function() {
+      // mielenhallintakakkara
+    },
+    f5: function() {
+      // hans
+    },
+    f6: function() {
+      // akku
+    },
+    f7: function() {
+      // miinaharava
+    },
+    f8: function() {
+      // gentoo
+    },
+    f9: function() {
+      // kärpät
+      window.close();
+    }
   }
-  var komennot = ['listaa kaikki', 'google', 'oma nimesi', 'responsiivisuus', 'mielenhallintakakkara', 'hans', 'akku', 'miinaharava', 'kärpät', 'gentoo'];
+  var komennot = ['listaa kaikki', 'google', 'oma nimesi', 'responsiivisuus', 'mielenhallintakakkara', 'hans', 'akku', 'miinaharava', 'gentoo', 'kärpät'];
   console.log("Valmis.");
-  APPI.f0();
+  //APPI.f0();
+  eval(`APPI.f${komennot.indexOf('listaa kaikki')}()`)
   // APPI.f{i}();
   // $(".APPI-inner").html(appi);
 });
