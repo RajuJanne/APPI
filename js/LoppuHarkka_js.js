6749ee9ca71bf6d27043829f7ebc8fe1ff204732
RajuJanne <moby@insomnia.fi>
2018-03-06 10:46:54 +0200

    luova tunti

---
+++ js/LoppuHarkka_js.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  var APPI = {
+    f0: function() {
+      // let googlepop = "<div id="googlePop">";
+      //     googlePop +=  "<img src='./images/kyngs.png' width=275px heigth=275px>";
+      console.log(komennot[0]);
+    },
+    f1: function() {
+
+    },
+    f2: function() {
+
+    },
+    f3: function() {
+
+    },
+  }
+  var komennot = ['listaa kaikki', 'google', 'oma nimesi', 'responsiivisuus', 'mielenhallintakakkara', 'hans', 'akku', 'miinaharava', 'kärpät', 'gentoo'];
+  console.log("Valmis.");
+  APPI.f0();
+  // APPI.f{i}();
+  // $(".APPI-inner").html(appi);
+});
+
+// APPI.google;
